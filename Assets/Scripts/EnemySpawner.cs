8d73511978e93042e77e4b45790599e4945e48f4
FranciscoPS <francisco.ps1996@gmail.com>
2026-05-01 13:05:15 -0600

    feat: colliders

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,8 +4,15 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
     [SerializeField] private GameObjectPool enemyBulletPool;
+    [SerializeField] private float spawnInterval = 3f;
 
     private void Start()
+    {
+        SpawnEnemy();
+        InvokeRepeating(nameof(SpawnEnemy), spawnInterval, spawnInterval);
+    }
+
+    private void SpawnEnemy()
     {
         GameObject instance = Instantiate(enemyPrefab, transform.position, transform.rotation);
         instance.GetComponent<EnemyAI>().SetPool(enemyBulletPool);
