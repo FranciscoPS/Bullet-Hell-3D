9ccb7a1560b40e69b7b846246315b112a47a6dd0
M-A-R-M <mauricio.reyna@saei.mx>
2026-05-29 16:14:00 -0600

    DeathAnim fix
    DeathAnim fix

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,33 +1,92 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField] private float maxHealth = 100f;
+
+    [Header("Death")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private float deathAnimationDuration = 2f;
+    [SerializeField] private GameObject visualRoot;
 
     public UnityEvent onDeath;
 
     private float currentHealth;
+    private bool isDead;
+
+    private PlayerMovement playerMovement;
 
     private void Awake()
     {
         currentHealth = maxHealth;
+
+        if (animator == null)
+            animator = GetComponentInChildren<Animator>();
+
+        playerMovement = GetComponent<PlayerMovement>();
     }
 
     public void TakeDamage(float amount)
     {
+        if (isDead)
+            return;
+
         currentHealth -= amount;
-        currentHealth = Mathf.Max(currentHealth, 0f);
-        Debug.Log($"[PlayerHealth] Vida: {currentHealth} / {maxHealth}");
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+
+        Debug.Log($"Vida: {currentHealth}/{maxHealth}");
 
         if (currentHealth <= 0f)
+        {
             Die();
+        }
     }
 
     private void Die()
     {
-        Debug.Log("[PlayerHealth] El jugador ha muerto.");
+        if (isDead)
+            return;
+
+        isDead = true;
+
+        Debug.Log("Jugador muerto");
+
         onDeath?.Invoke();
-        gameObject.SetActive(false);
+
+        if (playerMovement != null)
+            playerMovement.enabled = false;
+
+        if (animator != null)
+            animator.SetBool("isDead", true);
+
+        StartCoroutine(DeathRoutine());
+    }
+
+    private IEnumerator DeathRoutine()
+    {
+        yield return new WaitForSeconds(deathAnimationDuration);
+
+        if (visualRoot != null)
+            visualRoot.SetActive(false);
+
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = false;
+        }
+    }
+
+    public float GetHealth()
+    {
+        return currentHealth;
+    }
+
+    public bool IsDead()
+    {
+        return isDead;
     }
 }
