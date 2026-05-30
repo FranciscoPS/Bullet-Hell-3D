5caa9e90224136bae337b4eb057e0e36633c8c03
Pedro-DJC <pedro.juarez@saei.mx>
2026-05-29 20:31:53 -0600

    Transitions_Working

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+using System.Collections;
 
 public class SceneController : MonoBehaviour
 {
+    //Singleton
     public static SceneController Instance;
+
+    //Scene
+    [SerializeField] private CanvasGroup screenDarkener;
 
     void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
@@ -20,11 +26,18 @@ public class SceneController : MonoBehaviour
 
     public void StartGameScene()
     {
-        SceneManager.LoadScene("MovementTest");
+        StartCoroutine(FadeOutGameScene());
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    IEnumerator FadeOutGameScene()
+    {
+        screenDarkener.DOFade(1f, 1.5f);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("MovementTest");
     }
 }
